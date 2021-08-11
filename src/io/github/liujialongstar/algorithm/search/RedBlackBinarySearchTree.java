93c55f357390d005c0b96c4c0b5fcacb7c498937
Liu Jialong <liujialongstar@163.com>
2021-08-11 09:29:07 +0800

    Update RedBlackBinarySearchTree.java

--- src/io/github/liujialongstar/algorithm/search/RedBlackBinarySearchTree.java
+++ src/io/github/liujialongstar/algorithm/search/RedBlackBinarySearchTree.java
@@ -1,5 +1,7 @@
 package io.github.liujialongstar.algorithm.search;
 
+import io.github.liujialongstar.algorithm.LinkedQueue;
+
 /**
  * @author liujialong
  * @date 2021/8/5
@@ -7,6 +9,10 @@ package io.github.liujialongstar.algorithm.search;
  */
 public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements OrderedSymbolTable<K, V> {
     private Node root;
+
+    private static final boolean RED = true;
+
+    private static final boolean BLACK = false;
 
     /**
      * 将键值对存放再表中(若值为空则将键key从表中删除)
@@ -16,7 +22,7 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public void put(K key, V val) {
-        put(root, key, val);
+        root = put(root, key, val);
         root.color = BLACK;
     }
 
@@ -56,7 +62,21 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if(x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) {
+            return get(x.left, key);
+        }else if (cmp > 0) {
+            return get(x.right, key);
+        }else {
+            return x.val;
+        }
     }
 
     /**
@@ -66,7 +86,40 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public void delete(K key) {
+        if(!contains(key)){
+            return;
+        }
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
 
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    private Node delete(Node h, K key) {
+        // assert get(h, key) != null;
+
+        if (key.compareTo(h.key) < 0)  {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        }
+        else {
+            if (isRed(h.left))
+                h = rotateRight(h);
+            if (key.compareTo(h.key) == 0 && (h.right == null))
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            if (key.compareTo(h.key) == 0) {
+                Node x = min(h.right);
+                h.key = x.key;
+                h.val = x.val;
+                h.right = deleteMin(h.right);
+            }
+            else h.right = delete(h.right, key);
+        }
+        return balance(h);
     }
 
     /**
@@ -77,7 +130,7 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public boolean contains(K key) {
-        return false;
+        return get(key) != null;
     }
 
     /**
@@ -87,7 +140,7 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 
     /**
@@ -114,7 +167,15 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public K min() {
-        return null;
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if(x.left == null) {
+            return x;
+        }else {
+            return min(x.left);
+        }
     }
 
     /**
@@ -124,7 +185,15 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public K max() {
-        return null;
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if(x.right == null) {
+            return x;
+        }else {
+            return max(x.right);
+        }
     }
 
     /**
@@ -135,7 +204,30 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public K floor(K key) {
-        return null;
+        Node x = floor(root, key);
+        if(x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node floor(Node x, K key) {
+        if(x == null){
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) {
+            return floor(x.left, key);
+        }else if(cmp > 0) {
+            Node t = floor(x.right, key);
+            if(t != null) {
+                return t;
+            }else {
+                return x;
+            }
+        }else {
+            return x;
+        }
     }
 
     /**
@@ -146,29 +238,80 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public K ceiling(K key) {
-        return null;
+        Node x = ceiling(root, key);
+        if(x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node ceiling(Node x, K key) {
+        if(x == null) {
+            return null;
+        }
+        int cmp = x.key.compareTo(key);
+        if(cmp < 0) {
+            return ceiling(x.right, key);
+        }else if(cmp > 0) {
+            Node t = ceiling(x.left, key);
+            if(t != null) {
+                return t;
+            }else {
+                return x;
+            }
+        }else {
+            return x;
+        }
     }
 
     /**
      * 小于key的键的数量
-     *
+     * 这里的1是根结点
      * @param key
      * @return
      */
     @Override
     public int rank(K key) {
-        return 0;
+        return rank(root, key);
+    }
+
+    private int rank(Node x, K key) {
+        if(x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) {
+            return rank(x.left, key);
+        }else if(cmp > 0) {
+            return 1 + size(x.left) +rank(x.right, key);
+        }else {
+            return size(x.left);
+        }
     }
 
     /**
      * 排名为k的键
-     *
+     * 这里的1是根结点
      * @param k
      * @return
      */
     @Override
     public K select(int k) {
-        return null;
+        return select(root , k);
+    }
+
+    private K select(Node x, int k) {
+        if(x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if(t > k) {
+            return select(x.left, k);
+        }else if(t < k) {
+            return select(x.right, k - t - 1);
+        }else {
+            return x.key;
+        }
     }
 
     /**
@@ -176,7 +319,21 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public void deleteMin() {
+        root = deleteMin(root);
+        if(!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
 
+    private Node deleteMin(Node h) {
+        if (h.left == null) {
+            return null;
+        }
+        if (!isRed(h.left) && !isRed(h.left.left)) {
+            h = moveRedLeft(h);
+        }
+        h.left = deleteMin(h.left);
+        return balance(h);
     }
 
     /**
@@ -184,7 +341,32 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public void deleteMax() {
+        root = deleteMax(root);
+        if(!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
 
+    private Node deleteMax(Node h) {
+        if(isRed(h.left)) {
+            h = rotateRight(h);
+        }
+
+        if(h.right == null){
+            return null;
+        }
+
+        if(!isRed(h.right) && !isRed(h.right.left)){
+            h = moveRedRight(h);
+        }
+
+        h.right = deleteMax(h.right);
+        return balance(h);
+    }
+
+    @Override
+    public Iterable<K> keys() {
+        return keys(min(), max());
     }
 
     /**
@@ -196,17 +378,39 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
      */
     @Override
     public Iterable<K> keys(K low, K high) {
-        return null;
+        LinkedQueue<K> queue = new LinkedQueue<>();
+        keys(root, queue, low, high);
+        return queue;
     }
 
-    /**
-     * 表中所有键的集合, 已排序
-     *
-     * @return
-     */
-    @Override
-    public Iterable<K> keys() {
-        return null;
+    private void keys(Node x, LinkedQueue<K> queue, K low, K high){
+        if(x == null) {
+            return;
+        }
+        int cmpLow = low.compareTo(x.key);
+        int cmpHigh = high.compareTo(x.key);
+
+        // 递归调用小于low的左子树
+        if(cmpLow < 0) {
+            keys(x.left, queue, low, high);
+        }
+        // 如果当前结点落在low..high的范围内, 将当前节点的键压入队列
+        if(cmpLow <= 0 && cmpHigh >= 0) {
+            queue.enqueue(x.key);
+        }
+
+        // 递归调用大于high的右子树
+        if(cmpHigh > 0) {
+            keys(x.right, queue, low, high);
+        }
+    }
+
+    public int height() {
+        return height(root);
+    }
+    private int height(Node x) {
+        if (x == null) return -1;
+        return 1 + Math.max(height(x.left), height(x.right));
     }
 
     class Node {
@@ -247,10 +451,6 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
             this.color = color;
         }
     }
-
-    private static final boolean RED = true;
-
-    private static final boolean BLACK = false;
 
     private boolean isRed(Node x) {
         if(x == null) {
@@ -301,5 +501,45 @@ public class RedBlackBinarySearchTree<K extends Comparable<K>, V> implements Ord
         h.color = RED;
         h.left.color = BLACK;
         h.right.color = BLACK;
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node moveRedRight(Node h) {
+        flipColors(h);
+        if (isRed(h.left.left)) {
+            h = rotateRight(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node balance(Node h) {
+        if (isRed(h.right) && !isRed(h.left))    h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))     flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"};
+        RedBlackBinarySearchTree<String, Integer> st = new RedBlackBinarySearchTree<>();
+        for (int i = 0; i < a.length; i++) {
+            String key = a[i];
+            st.put(key, i);
+        }
+        for(String s: st.keys()){
+            System.out.println(s + ": " + st.get(s));
+        }
     }
 }
